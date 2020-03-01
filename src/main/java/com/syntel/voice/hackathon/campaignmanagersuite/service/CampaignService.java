dfcb952e1c0a90274fcb1916d7ed0b5578ed91a1
Prabhdeep <Prabhdeep@Users-MacBook-Pro.local>
2020-03-01 16:07:18 +0000

    publish, unpublish and create logic

--- src/main/java/com/syntel/voice/hackathon/campaignmanagersuite/service/CampaignService.java
+++ src/main/java/com/syntel/voice/hackathon/campaignmanagersuite/service/CampaignService.java
@@ -1,9 +1,6 @@
 package com.syntel.voice.hackathon.campaignmanagersuite.service;
 
-import com.google.actions.api.ActionRequest;
-import com.google.actions.api.ActionResponse;
-import com.google.actions.api.DialogflowApp;
-import com.google.actions.api.ForIntent;
+import com.google.actions.api.*;
 import com.google.actions.api.response.ResponseBuilder;
 import com.syntel.voice.hackathon.campaignmanagersuite.exception.CampaignException;
 import com.syntel.voice.hackathon.campaignmanagersuite.representation.Campaign;
@@ -14,8 +11,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CampaignService extends DialogflowApp {
@@ -23,11 +20,14 @@ public class CampaignService extends DialogflowApp {
     private static final Logger LOGGER = LoggerFactory.getLogger(CampaignService.class);
 
     List<Campaign> campaigns = new ArrayList<>();
-    List<String> getTasks = Arrays.asList("list", "fetch", "get", "grab", "hear", "see");
+    List<String> activeStatus = Arrays.asList("active", "live", "published");
+    List<String> inActiveStatus = Arrays.asList("inactive", "unpublished");
+    List<String> publishTasks = Arrays.asList("publish", "schedule");
+    List<String> unpublishTasks = Arrays.asList("list", "fetch", "get", "grab", "hear", "see");
 
-    @ForIntent("user_operations")
+    @ForIntent("get_active_inactive_campaigns")
     public ActionResponse make_name(ActionRequest request) {
-        LOGGER.info("user_operations");
+        LOGGER.info("get_active_inactive_campaigns");
 
         String task = request.getParameter("task").toString(); //add, fetch, get, publish, unpublish, delete
         String campStatus = request.getParameter("camp_status").toString(); //active, inactive, scheduled,
@@ -35,50 +35,96 @@ public class CampaignService extends DialogflowApp {
         String response;
 
         try {
-            response = executeAction(task, campStatus);
+            if (StringUtils.isEmpty(campStatus)) {
+                campStatus = "active";
+            }
+
+            if (StringUtils.isEmpty(task)) {
+                task = "list";
+            }
+
+            if (activeStatus.contains(campStatus)) { //list campaigns
+                String campaigns = getCampaignsByStatus("active").toString();
+                response = "Hi these are your " + campStatus + " campaigns " + campaigns;
+                response = response.concat("\n What do you want to do next, you can ");
+                response = response.concat("deactivate the campaign");
+            }
+            else  if (inActiveStatus.contains(campStatus)) {
+                String campaigns = getCampaignsByStatus("inactive").toString();
+                response = "Hi these are your " + campStatus + " campaigns " + campaigns;
+                response = response.concat("\n What do you want to do next, you can ");
+                response = response.concat("activate the campaign");
+            }
+            else if ("dev".equalsIgnoreCase(task)) {
+                response =  clearCampaigns() + addTestCampaigns();
+            } else {
+                throw new CampaignException();
+            }
         } catch (CampaignException ce) {
             ce.printStackTrace();
-            response = "Exception aai re";
+            response = "Sorry, could I not understand your request !";
         }
 
         ResponseBuilder responseBuilder = getResponseBuilder(request).add(response);
         ActionResponse actionResponse = responseBuilder.build();
         LOGGER.info(actionResponse.toString());
-        LOGGER.info("make_name end.");
+        LOGGER.info("get_active_inactive_campaigns end.");
         return actionResponse;
     }
 
-    @ForIntent("prepare_dev_env")
-    public ActionResponse prepareDevEnv(ActionRequest request) {
-        LOGGER.info("prepareDevEnv");
+    @ForIntent("publish_or_unpublish_campaign")
+    public ActionResponse askForUserCommand(ActionRequest request) {
+        LOGGER.info("publish_or_unpublish_campaign");
 
-        String response = clearCampaigns() + addTestCampaigns();
-        ResponseBuilder responseBuilder = getResponseBuilder(request).add(response);
-        ActionResponse actionResponse = responseBuilder.build();
-        LOGGER.info(actionResponse.toString());
-        LOGGER.info("prepareDevEnv end.");
-        return actionResponse;
-    }
+        ActionContext context = request.getContext("awaiting_publish_or_unpublish_id");
+        LOGGER.info("Got context with name {}", context.getName());
+        Map parameters = context.getParameters();
 
-    private String executeAction(String task, String campStatus) {
-        if (StringUtils.isEmpty(campStatus)) {
-            campStatus = "active";
-        }
-        if (getTasks.contains(task)) { //list campaigns
-            String campaigns = getCampaignsByStatus(campStatus).toString();
-            String response = "Hi these are your " + campStatus + "campaigns" + campaigns;
-            response = response.concat("\n What do you want to do next, you can");
-            if (campStatus.equalsIgnoreCase("active")) {
-                response = response.concat("either publish or deactivate the campaign");
+        String task = parameters.get("campaign_task").toString();
+
+        int campaignId = (int) request.getParameter("campaign_id");
+
+        String response;
+
+        try {
+            if(publishTasks.contains(task)) {
+                campaigns.get(campaignId).setStatus("active");
+                response = "Campaign with ID " + campaignId + " has been published";
+            } else if(unpublishTasks.contains(task)) {
+                campaigns.get(campaignId).setStatus("inactive");
+                response = "Campaign with ID " + campaignId + " has been unpublished";
+            } else {
+                throw new CampaignException();
             }
-            return response;
-        } else if ("publish".equalsIgnoreCase(task)) {
-            return "Campaign Published";
-        } else if ("dev".equalsIgnoreCase(task)) {
-            return clearCampaigns() + addTestCampaigns();
+        } catch (CampaignException ce) {
+            ce.printStackTrace();
+            response = "Sorry, could I not understand your request !";
         }
 
-        throw new CampaignException();
+        ResponseBuilder responseBuilder = getResponseBuilder(request).add(response);
+        ActionResponse actionResponse = responseBuilder.build();
+        LOGGER.info(actionResponse.toString());
+        LOGGER.info("publish_or_unpublish_campaign end.");
+        return actionResponse;
+    }
+
+    @ForIntent("create_campaign")
+    public ActionResponse createCampaign(ActionRequest request) {
+        LOGGER.info("create_campaign");
+
+        String title = request.getParameter("title").toString();
+        String description = request.getParameter("description").toString();
+
+        int ID = campaigns.size()+1;
+        new Campaign(ID, title, description, "inactive");
+
+        String response = "Your campaign has been created with ID" + ID;
+
+        ResponseBuilder responseBuilder = getResponseBuilder(request).add(response);
+        ActionResponse actionResponse = responseBuilder.build();
+        LOGGER.info(actionResponse.toString());
+        LOGGER.info("create_campaign end.");
+        return actionResponse;
     }
 
     private List<String> getCampaignsByStatus(String status) {
@@ -99,14 +145,6 @@ public class CampaignService extends DialogflowApp {
         return camps;
     }
 
-    private String addCampaign(String campaignName, String campaignText) {
-        //int iSize = dataStore.getInactiveCampaigns().size();
-        //dataStore.getInactiveCampaigns().add(new Campaign(iSize + 1, campaignName, campaignText));
-
-        return "Campaign Added to inactive campaigns";
-        //TODO : Logic to ask if you want to schedule this campaign
-    }
-
     private String clearCampaigns() {
         campaigns = new ArrayList<>();
         return "Campaigns cleared.";
@@ -115,9 +153,9 @@ public class CampaignService extends DialogflowApp {
     private String addTestCampaigns() {
 
         Campaign campaign1 = new Campaign(1, "Hackathon Test", "This is the test hackathon campaign", "active");
-        Campaign campaign2 = new Campaign(2, "Banking Test", "This is the test banking campaign", "deactive");
-        Campaign campaign3 = new Campaign(2, "healthcare Test", "This is the test healthcare campaign", "active");
-        Campaign campaign4 = new Campaign(2, "manufacturing Test", "This is the test manufacturing campaign", "deactive");
+        Campaign campaign2 = new Campaign(2, "Banking Test", "This is the test banking campaign", "inactive");
+        Campaign campaign3 = new Campaign(3, "healthcare Test", "This is the test healthcare campaign", "active");
+        Campaign campaign4 = new Campaign(4, "manufacturing Test", "This is the test manufacturing campaign", "inactive");
 
         campaigns.add(campaign1);
         campaigns.add(campaign2);
